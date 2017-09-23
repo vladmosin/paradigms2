cde03dcf9873db8eece252c88ec3c31fd60851fe
Vladislav Mosin <vmos1999@mail.ru>
2017-09-23 14:43:17 -0400

    homework

--- python.py
+++ python.py
@@ -4,22 +4,24 @@ import sys
 from os import path
 from hashlib import sha1 as hasher
 	
-	
-s = input()
-d = dict()
-hasher = hasher()
-for path, _, files in walk(s):	
-	for f_names in files:
-		if f_files[0] != '.' and f.files[0] != '~' :
-			n = h1.update(path +'/' + f_names).hexdigest()
-			if n not in d:
-				d[n] = [1, path + '/' + f_names]
-			else:
-				d[n][0] = 2
-				d[n][1] += ":" + path + '/' + f_names
-for hsh in d:
-	if d[hsh][0] == 2:
-		print(d[hsh][1] + '\n')
+def main():	
+	s = sys.argv[1]
+	d = dict()
+	for path, _, files in walk(s):	
+		for f_names in files:
+			if f_files[0] != '.' and f.files[0] != '~' :
+				with open(os.path.join(path, f_names), mode = 'rb') as f:
+				hasher = hasher()
+				n = hasher.update(f.read()).hexdigest()
+				if n not in d:
+					d[n] = [1, path + '/' + f_names]
+				else:
+					d[n][0] = 2
+					d[n][1] += ":" + path + '/' + f_names
+	for hsh in d:
+		if d[hsh][0] == 2:
+			print(d[hsh][1] + '\n')
+if '__name__' == __main__
 				
 				
 			
