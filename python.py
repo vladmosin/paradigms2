38935518b3e1c3a9007c9be5ca3f94b48a586486
Vladislav Mosin <vmos1999@mail.ru>
2017-10-14 05:47:48 -0400

    commit

--- python.py
+++ python.py
@@ -4,25 +4,38 @@ import sys
 from os import path
 from hashlib import sha1 as hasher
 	
+	
+def hashing(s1):
+	h1= hasher()
+	with open(s1, mode = 'rb') as f:
+		while True:
+			s2 = f.read(4096)
+			if not s2:
+				break
+			h1.update(s2)
+	return h1.digest()
+	
+	
 def main():	
 	s = sys.argv[1]
-	d = dict()
+	h1 = hasher()
+	way = []
 	for path, _, files in walk(s):	
-		for f_names in files:
-			if f_files[0] != '.' and f.files[0] != '~' :
-				with open(os.path.join(path, f_names), mode = 'rb') as f:
-				hasher = hasher()
-				n = hasher.update(f.read()).hexdigest()
-				if n not in d:
-					d[n] = [1, path + '/' + f_names]
-				else:
-					d[n][0] = 2
-					d[n][1] += ":" + path + '/' + f_names
-	for hsh in d:
-		if d[hsh][0] == 2:
-			print(d[hsh][1] + '\n')
-if '__name__' == __main__
-				
+		for f in files:
+			if f[0] != '.' and f[0] != '~' and not os.path.islink(f):
+				way.append(os.path.join(path, f))
+	d = {}
+	for p in way:
+		k = hashing(p)
+		if k not in d:
+			d[k] = [p]
+        else:
+			d[k].append(p)
+	for hsh in d.values():
+		if len(hsh) > 1:
+			print(":".join(hsh))
+if __name__ == "__main__":
+	main()		
 				
 			
 		
